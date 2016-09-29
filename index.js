c05c93b897d794daaae4652df8629e35204bb1f4
Matthew W <matthew.woolf@mailonline.co.uk>
2016-09-29 02:35:25 +0100

    Initial commit - Implement memo(afn,options)

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Async FunctioN utilities */
+
+/* Each function is in a separate file for easy pulling apart if you only want a subset */
+
+module.exports = {
+    memo:require('./lib/memo')
+} ;
---
+++ test/test.js
@@ -0,0 +1,35 @@
+"use nodent";
+
+var afn = require('..') ;
+
+async function sleep(t) {
+    setTimeout(function(){ async return },t) ;
+}
+
+async function doubleArg1(n) {
+    console.log("doubleArg1(",n,")") ;
+    await sleep(500) ;
+    return n*2 ;
+}
+
+/* memo */
+var memo = afn.memo(doubleArg1,{ttl:2000, key(self,args,fn){ return args[0] }})
+console.log("Test memoized") ;
+
+console.log(await memo(10,"a")) ;
+console.log(await memo(10,"b")) ;
+console.log(await memo(11,"a")) ;
+await sleep(100);
+console.log(await memo(11,"b")) ;
+await sleep(3000);
+console.log(await memo(10,"a")) ;
+console.log(await memo(11,"a")) ;
+console.log(await memo(10,"a")) ;
+console.log(await memo(11,"a")) ;
+
+console.log(await Promise.all([
+   memo(10,"a"),                               
+   memo(10,"a"),                               
+   memo(11,"a"),                               
+   memo(11,"a")                               
+])) ;
