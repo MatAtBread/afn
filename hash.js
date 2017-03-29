0c7cecdc0b197056930781f1083325f99e0284c2
Matthew W <matthew.woolf@mailonline.co.uk>
2017-03-29 14:53:46 +0100

    Change "o instanceof Object" to "typeof o === 'object'" to avoid cross-frame weirdness in browsers

--- hash.js
+++ hash.js
@@ -11,7 +11,7 @@ module.exports = function(config){
             h.update("null") ;
             return ;
         }
-        if (o instanceof Object){
+        if (typeof o === 'object'){
             if (m.get(o))
                 return ;
             m.set(o,o) ;
