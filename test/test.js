a0be0ae6e448fc81b429b101d0738673dc5cdd72
matthew.woolf@mailonline.co.uk <matthew.woolf@mailonline.co.uk>
2019-04-24 17:19:48 +0100

    Clear file cache before running

--- test/test.js
+++ test/test.js
@@ -3,14 +3,14 @@
 
 const delay = 500 ;
 
-async function sleep(t) {
-    setTimeout(function(){ async return },t) ;
+function sleep(t) {
+    return new Promise(resolve => setTimeout(resolve, t));
 }
 
 var passes = 0, fails = 0 ;
 async function memoize(name,memo) {
     var output = [] ;
-
+    
     function log(s) {
         output.push(s) ;
         if (s==='done') {
@@ -32,34 +32,34 @@ async function memoize(name,memo) {
         ttl:60000, 
         key:function(self, args, asyncFunction) { return args[0] }  
     }) ;
-
+    
     async function test(x) {
         log("run") ;
         if (x[0] === '!')
-            throw ("NoCache-"+x) ;
+        throw ("NoCache-"+x) ;
         await sleep(delay) ;
         return x ;
     }
-
+    
     const localTest = memo(test,{
         ttl:60000, 
         key:function(self, args, asyncFunction) { return args[0] }  
     }) ;
-
+    
     immediateTest("first").then(log,log) ;
-
+    
     localTest("first").then(log,log) ;
     localTest("second").then(log,log) ;
     localTest("first").then(log,log) ;
     await sleep(delay*1.2);
     localTest("first").then(log,log) ;
     localTest("second").then(log,log) ;
-
+    
     localTest("!").then(log,log) ;
     await sleep(delay*1.2);
     localTest("!").then(log,log) ;
     localTest("!").then(log,log) ;
-
+    
     immediateTest("first").then(log,log) ;
     
     localTest("done").then(log,log) ;
@@ -72,7 +72,7 @@ await memoize('local',memo()) ;
 // Test afn-redis-cache
 try {
     const redisCache = require('afn-redis-cache')({
-//        log(){console.log('afn-redis-cache',Array.prototype.slice.call(arguments).toString())},
+        //        log(){console.log('afn-redis-cache',Array.prototype.slice.call(arguments).toString())},
         redis:"redis://127.0.0.1/13",
         defaultTTL:120,
         asyncTimeOut:30
@@ -117,18 +117,14 @@ await memoize('async',memo({
 })) ;
 
 //Test a file-based cache
-const fs = require('fs') ;
 await memoize('file',memo({
     crypto:"basicCreateHash",
     createCache:function(id){
-        var dir = "./afn-data/" ;
-        try {
-            fs.mkdirSync(dir) ;
-        } catch (ex) {
-            if (ex.code !== "EEXIST")
-                throw ex ;
-        }
-        return {
+        const fs = require('fs') ;
+        const root = "./afn-data/" ;
+        const dir = root+id+"/" ;
+
+        var fileCache = {
             get(key) { 
                 try { 
                     return JSON.parse(fs.readFileSync(dir+encodeURIComponent(key))) 
@@ -138,8 +134,8 @@ await memoize('file',memo({
                 } 
             },
             set(key,value) { fs.writeFileSync(dir+encodeURIComponent(key),JSON.stringify(value)) },
-            keys() { return fs.readdirSync(dir) },
-            clear() { fs.unlinkSync(dir+"*") ; fs.rmdirSync(dir) },
+            keys() { try { return fs.readdirSync(dir).map(n => decodeURIComponent(n)) } catch (ex) { return [] }},
+            clear() { fileCache.keys().forEach(fn => fileCache.delete(fn)) },
             'delete'(key) {
                 try {
                     fs.unlinkSync(dir+encodeURIComponent(key)) ;
@@ -149,6 +145,12 @@ await memoize('file',memo({
                 }
             }
         }
+        
+        console.log("Deleting old file cache",id);
+        fileCache.keys().forEach(fn => fileCache.delete(fn)) ;
+        try { fs.mkdirSync(root) ; } catch(ex) {}
+        try { fs.mkdirSync(dir) ; } catch(ex) {}
+        return fileCache ;
     }
 })) ;
 
